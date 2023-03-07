a5649f41da04388061659e1ad2252eba72a9b292
Ethan Pippin <ethanpippin2343@gmail.com>
2023-03-06 22:07:55 -0700

    Update GeneratePlugin.swift

--- Plugins/CreateAPI/GeneratePlugin.swift
+++ Plugins/CreateAPI/GeneratePlugin.swift
@@ -11,21 +11,49 @@ import PackagePlugin
 
 @main
 struct Plugin: CommandPlugin {
-    func performCommand(context: PluginContext, arguments: [String]) async throws {
-        let createAPICommand = try context.tool(named: "create-api")
-        let workingDirectory = context.package.directory.appending("Sources")
+    
+    private func runProcess(_ commandLine: String, context: PluginContext, workingDirectory: String? = nil) throws {
+        
+        var arguments = commandLine.split(separator: " ").map { String($0) }
+        let commandName = arguments.removeFirst()
+        
+        let command = try context.tool(named: commandName)
+        
+        let actualWorkingDirectory: Path
+        
+        if let workingDirectory {
+            actualWorkingDirectory = context.package.directory.appending(workingDirectory)
+        } else {
+            actualWorkingDirectory = context.package.directory
+        }
 
         let process = Process()
-        process.currentDirectoryURL = URL(fileURLWithPath: workingDirectory.string)
-        process.executableURL = URL(fileURLWithPath: createAPICommand.path.string)
-        process.arguments = [
-            "generate",
-            "jellyfin-openapi-stable.json",
-            "--config", "create-api-config.yaml",
-            "--output", ".",
-        ]
-
+        process.currentDirectoryURL = URL(fileURLWithPath: actualWorkingDirectory.string)
+        process.executableURL = URL(fileURLWithPath: command.path.string)
+        process.arguments = arguments
+        
         try process.run()
         process.waitUntilExit()
+    }
+    
+    private func generate(context: PluginContext) async throws {
+        try runProcess(
+            "create-api generate jellyfin-openapi-stable.json --config create-api-config.yaml --output .",
+            context: context,
+            workingDirectory: "Sources"
+        )
+    }
+    
+    private func lint(context: PluginContext) async throws {
+        try runProcess(
+            "swiftformat .",
+            context: context
+        )
+    }
+    
+    func performCommand(context: PluginContext, arguments: [String]) async throws {
+        
+        try await generate(context: context)
+        try await lint(context: context)
     }
 }
