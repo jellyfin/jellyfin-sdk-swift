0eb8751ef7e5551d8fd6eb17b4d89aa8d34ec1e8
Joe Kribs <jpkribs@outlook.com>
2025-10-20 14:42:09 -0600

    dovi vs dOVI

--- Sources/Entities/VideoRangeType.swift
+++ Sources/Entities/VideoRangeType.swift
@@ -18,9 +18,9 @@ public enum VideoRangeType: String, Codable, CaseIterable {
     case doviWithHDR10 = "DOVIWithHDR10"
     case doviWithHLG = "DOVIWithHLG"
     case doviWithSDR = "DOVIWithSDR"
-    case dOVIWithEL = "DOVIWithEL"
-    case dOVIWithHDR10Plus = "DOVIWithHDR10Plus"
-    case dOVIWithELHDR10Plus = "DOVIWithELHDR10Plus"
-    case dOVIInvalid = "DOVIInvalid"
+    case doviWithEL = "DOVIWithEL"
+    case doviWithHDR10Plus = "DOVIWithHDR10Plus"
+    case doviWithELHDR10Plus = "DOVIWithELHDR10Plus"
+    case doviInvalid = "DOVIInvalid"
     case hdr10Plus = "HDR10Plus"
 }
